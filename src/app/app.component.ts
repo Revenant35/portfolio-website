5ae2aa231ad0f80d59f350abf00ae2326fc0a44b
Zach Brown <bbrown64506@gmail.com>
2024-02-11 15:55:56 -0600

    Removing background component, moving it into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {BackgroundComponent} from "./background/background.component";
+import {PulseCardComponent} from "./pulse-card/pulse-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BackgroundComponent],
+  imports: [RouterOutlet, PulseCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
+  lagDuration = 400; // ms
+
+  @ViewChild('blob') blob: ElementRef | undefined;
+
+  constructor() { }
+
+  @HostListener('document:pointerout', ['$event'])
+  onPointerOut(_event: Event) {
+    if (!this.blob) {
+      throw new Error('No blob element found');
+    }
+    this.blob.nativeElement.style.opacity = '0';
+  }
+
+  @HostListener('document:pointerenter', ['$event'])
+  onPointerEnter(_event: Event) {
+    if (!this.blob) {
+      throw new Error('No blob element found');
+    }
+    this.blob.nativeElement.style.opacity = '0.6';
+  }
+
+  @HostListener('document:pointermove', ['$event'])
+  onPointerMove(event: MouseEvent) {
+    if (!this.blob) {
+      throw new Error('No blob element found');
+    }
+
+    let {clientX, clientY} = event;
+    const blobElement = this.blob.nativeElement;
+
+    blobElement.style.opacity = '0.6';
+
+    blobElement.animate({
+      left: `${clientX}px`,
+      top: `${clientY}px`
+    }, {duration: this.lagDuration, fill: 'forwards'});
+  }
 }
